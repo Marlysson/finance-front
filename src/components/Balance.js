704089d78c8193289c2eec1c6e06288f457087c6
Marlysson <marlysson5@gmail.com>
2021-04-30 18:22:48 -0300

    Balance component showing incomes and expenses values.

---
+++ src/components/Balance.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import '../styles/balance.css';
+
+class Balance extends React.Component {
+
+	render(){
+
+		return (
+			<div className="balance-container">
+				
+				<div className="balance-type">
+					<p class="balance-type-label">Receitas</p>
+					<p class="balance-type-value balance-incomes">R$ 1230.40</p>
+				</div>
+
+				<div className="balance-type">
+					<p class="balance-type-label">Despesas</p>
+					<p class="balance-type-value balance-expenses">R$ 650.00</p>
+				</div>
+
+			</div>
+		);
+	}
+
+}
+
+export default Balance;
