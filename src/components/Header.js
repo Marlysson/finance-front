057085cf2e92e11ab8de153b6d92b4ac0e6d357a
Marlysson <marlysson5@gmail.com>
2021-04-30 18:22:12 -0300

    Header component with all balance.

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Balance from './Balance.js';
+
+import '../styles/header.css';
+
+class Header extends React.Component {
+
+	render(){
+
+		return (
+			<div className="header-container">
+				
+				<h1 className="title">Minhas Finanças</h1>
+				<p className="balance-label">Meu saldo</p>
+				<p className="balance-value">R$ 458.00</p>
+
+				<Balance/>
+
+			</div>
+		);
+	}
+
+}
+
+export default Header;
