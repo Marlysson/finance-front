ebe8fcbb8c846d46ded342a3c56d883cd730ed46
Marlysson <marlysson5@gmail.com>
2021-05-08 19:15:01 -0300

    Refactoring ItemList component: conditional to show pagination button just when moviments was filled, moved pagination icons to ItemList component.

--- src/App.js
+++ src/App.js
@@ -12,14 +12,16 @@ class App extends React.Component{
 		super(props);
 		this.state = {
 			moviments : [],
-			value: '',
+			description: '',
+			amount: '',
 			operation: 'income'
 		}
 
 		this.onSubmit = this.onSubmit.bind(this);
 		this.onChange = this.onChange.bind(this);
 		this.onTypingValue = this.onTypingValue.bind(this);
-
+		this.onCreateMoviment = this.onCreateMoviment.bind(this);
+		this.onDescriptionChange = this.onDescriptionChange.bind(this);
 	}
 
 	componentDidMount(){
@@ -31,31 +33,43 @@ class App extends React.Component{
 		})
 	}
 
+	onDescriptionChange(event){
+		this.setState({description: event.target.value});
+	}
+
 	onTypingValue(valueTyped){
-		this.setState({value: valueTyped.value})
+		this.setState({amount: valueTyped.value});
 	}
 
 	onChange(event){
-		this.setState({operation: event.target.value});
+		this.setState({ operation: event.target.value});
 	}
 
 	onSubmit(event){
 		event.preventDefault();
+	}
 
-		let data = JSON.stringify({
-			'description': 'Freela',
-			'amount': this.state.value,
-			'operation': this.state.operation.toUpperCase()
-		})
+	onCreateMoviment(){
+
+		let moviment = {
+			"amount": this.state.amount,
+			"description": this.state.description,
+			"operation": this.state.operation.toUpperCase()
+		}
+		
+		let data = JSON.stringify({ moviment });
 
 		let headers = {
 			'Content-Type': 'application/json;charset=UTF-8'
 		}
-
+		console.log(moviment);
+		console.log(data);
+		
 		fetch('http://localhost:8000/moviments/',
 			{ method: 'POST', body: data , headers: headers }
 		).then(response => response.json())
 		 .then((response) => {
+			 console.log(response)
 			alert(`Montante de ${response.amount} registrado com sucesso.`)
 			this.setState((state) => {
 				return { moviments: [response, ...state.moviments]};
@@ -64,7 +78,9 @@ class App extends React.Component{
 		}).catch(function(error){
 			console.log(error)
 		})
+
 	}
+
 	render(){
 
 		let { moviments }  = this.state;
@@ -81,81 +97,93 @@ class App extends React.Component{
 		    	</div>
 
 				<div className="modal fade" id="novoMovimento">
-					<div className="modal-dialog">
+					
+					<div className="modal-dialog modal-dialog-centered modal-sm">
+						
 						<div className="modal-content">
+							
 							<div className="modal-header">
 								
 								<div className="d-flex flex-direction-row justify-content-center" style={{'color':'#9B59B6', 'fontWeight': 'bold'}}>
+									
 									<span className="material-icons">monetization_on</span>
 									<h5 className="modal-title pl-2">
 										NOVO MOVIMENTO
 									</h5>
+
 								</div>
 
-								<button type="button" className="close" data-dismiss="modal" aria-label="Close"></button>
+								<button type="button" className="close" data-dismiss="modal"></button>
+
 							</div>
+
 							<form onSubmit={this.onSubmit}>
 
 								<div className="modal-body">
-	
-									<div className="row">
 
-										<div className="form-group col-lg-6">
+									<div className="form-group">
 
-											<label htmlFor="valor">Valor</label>
+										<label htmlFor="valor">Descrição</label>
+
+										<input value={this.state.description} onChange={this.onDescriptionChange} className="form-control form-control-sm"/>
+									
+									</div>
+
+									<div className="form-group">
+
+										<label htmlFor="valor">Valor</label>
+										
+										<div className="input-group input-group-sm">
 											
-											<div className="input-group">
-												
-												<div className="input-group-prepend">
-													<span className="input-group-text" id="basic-addon1">R$</span>
-													<NumberFormat 
-														value={this.state.value}
-														className="form-control"
-														onValueChange={this.onTypingValue}
-														isNumericString={true}
-														decimalScale={2}
-														fixedDecimalScale={true}
-														thousandSeparator={'.'}
-														decimalSeparator={','}
-													/>
-												</div>
-
+											<div className="input-group-prepend">
+												<span className="input-group-text">R$</span>
 											</div>
 											
+											<NumberFormat 
+												value={this.state.amount}
+												className="form-control"
+												onValueChange={this.onTypingValue}
+												isNumericString={true}
+												decimalScale={2}
+												fixedDecimalScale={true}
+												thousandSeparator={'.'}
+												decimalSeparator={','}
+											/>
+
+										</div>
+										
+									</div>
+
+									<div className="form-group d-flex mt-2 justify-content-around">
+
+										<div className="form-check">
+											<input 
+												className="form-check-input" 
+												type="radio" 
+												name="tipo" 
+												value="income" 
+												id="receita"
+												checked={this.state.operation === 'income'}
+												onChange={this.onChange}
+											/>
+											<label className="form-check-label" htmlFor="receita">
+												Receita
+											</label>
 										</div>
 
-										<div className="form-group col-lg-5 d-flex mt-4 justify-content-between">
-
-											<div className="form-check">
-												<input 
-													className="form-check-input" 
-													type="radio" 
-													name="tipo" 
-													value="income" 
-													id="receita"
-													checked={this.state.operation === 'income'}
-													onChange={this.onChange}
-												/>
-												<label className="form-check-label" htmlFor="receita">
-													Receita
-												</label>
-											</div>
-
-											<div className="form-check">
-												<input 
-													className="form-check-input" 
-													type="radio" 
-													name="tipo" 
-													id="despesa" 
-													value="expense" 
-													checked={this.state.operation === 'expense'}
-													onChange={this.onChange}
-												/>
-												<label className="form-check-label" htmlFor="despesa">
-													Despesa
-												</label>
-											</div>
-
+										<div className="form-check">
+											<input 
+												className="form-check-input" 
+												type="radio" 
+												name="tipo" 
+												id="despesa" 
+												value="expense" 
+												checked={this.state.operation === 'expense'}
+												onChange={this.onChange}
+											/>
+											<label className="form-check-label" htmlFor="despesa">
+												Despesa
+											</label>
 										</div>
 
 									</div>
@@ -164,7 +192,7 @@ class App extends React.Component{
 								
 								<div className="modal-footer">
 									<button type="button" className="btn btn-sm btn-secondary" data-dismiss="modal">Fechar</button>
-									<button type="button" type="submit" className="btn btn-sm custom-button">Salvar</button>
+									<button type="button" onClick={this.onCreateMoviment} className="btn btn-sm custom-button">Salvar</button>
 								</div>
 
 							</form>
@@ -173,16 +201,7 @@ class App extends React.Component{
 				</div>
 
 		    	<div className="container-sized ">
-		    		<ul className="list-group">
 					  <ItemList moviments={moviments} />
-					</ul>
-		    	</div>
-
-		    	<div className="container-sized mt-2 d-flex justify-content-center">
-		    		<div className="d-flex justify-content-between container-pagination">
-			    		<Button icon='arrow_back'/>
-			    		<Button icon='arrow_forward'/>
-		    		</div>
 		    	</div>
 
 		    </div>
--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../styles/itemlist.css';
+import Button from './Button';
 
 class ItemList extends React.Component{
 
@@ -9,21 +10,51 @@ class ItemList extends React.Component{
 
 	render(){
 
-		return (
+		let elements;
+
+		if ( this.props.moviments.length === 0){
+			elements = <li className="list-group-item mb-4 d-flex justify-content-center" style={{'fontWeight': 'bold'}}>Não há movimentações</li>;
+
+		}else{
 			
-			this.props.moviments.map((moviment) => 
+			elements = this.props.moviments.map((moviment) => 
+
+				<li key={moviment.id} className="list-group-item mb-2 d-flex justify-content-between">
+						
+						<span>{moviment.description}</span>
+						
+						<span className={`item-list-value ${'INCOME' === moviment.operation ? 'incomes-type' : 'expenses-type'}`}>
+							R$ {moviment.amount}
+						</span>
+				</li>	
 				
-				<li key={moviment.description} className="list-group-item mb-2 d-flex justify-content-between">
-				  	
-				  	<span>{moviment.description}</span>
-				  	
-				  	<span className={`item-list-value ${'INCOME' === moviment.operation ? 'incomes-type' : 'expenses-type'}`}>
-				  		R$ {moviment.amount}
-				  	</span>
-				</li>		
 			)
+		}
+		
+		return (
+
+			<div>
+
+				<ul className='list-group'>{elements}</ul>
+				
+				{
+				
+					this.props.moviments.length > 0 && 
+				
+					<div className="container-sized mt-2 d-flex justify-content-center">
+						<div className="d-flex justify-content-between container-pagination">
+							<Button icon='arrow_back'/>
+							<Button icon='arrow_forward'/>
+						</div>
+					</div>
+				}
+
+			</div>
+
 		)
+
 	}
+
 }
 
 export default ItemList;
